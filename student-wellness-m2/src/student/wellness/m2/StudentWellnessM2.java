e14d06225fefdadd8f8dba59a825e4ce57f22e5b
Sinoyolo Mabuza <Mabuza834@gmail.com>
2025-07-15 15:33:11 +0200

    Added the correct file and Set up file and apache Signed-off-by:Sinoyolo Mabuza <Mabuza834@gmail.com>

---
+++ student-wellness-m2/src/student/wellness/m2/StudentWellnessM2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.wellness.m2;
+
+/**
+ *
+ * @author mabuz
+ */
+public class StudentWellnessM2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
