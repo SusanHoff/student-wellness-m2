7de257ca7f25928d178b474257f5d69a876a67d1
SusanHoff <136302900+SusanHoff@users.noreply.github.com>
2025-07-16 22:02:56 +0200

    Added DataAccessObject package and updated Counselor code in View Model and Controller packages.

--- src/Controller/CounselorController.java
+++ src/Controller/CounselorController.java
@@ -8,6 +8,49 @@ package Controller;
  *
  * @author susan
  */
+
+import DataAccessObject.CounselorDAO;
+import Model.Counselor;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CounselorController {
-    
+  private CounselorDAO dao = new CounselorDAO();
+
+    public void addCounselor(int id, String name, String surname, String email, String phone,
+                             String password, String specialisation, String available) {
+        try {
+            Counselor c = new Counselor(id, name, surname, email, phone, password, specialisation, available);
+            dao.addCounselor(c);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateCounselor(int id, String name, String surname, String email, String phone,
+                                String password, String specialisation, String available) {
+        try {
+            Counselor c = new Counselor(id, name, surname, email, phone, password, specialisation, available);
+            dao.updateCounselor(c);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteCounselor(int id) {
+        try {
+            dao.deleteCounselor(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Counselor> getAllCounselors() {
+        try {
+            return dao.getAllCounselors();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
 }
--- src/View/CounselorForm.java
+++ src/View/CounselorForm.java
@@ -4,10 +4,87 @@
  */
 package View;
 
+import Controller.CounselorController;
+import Model.Counselor;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author susan
  */
-public class CounselorForm {
-    
+ 
+public class CounselorForm extends javax.swing.JFrame {
+
+    CounselorController controller = new CounselorController();
+
+    public CounselorForm() {
+        initComponents();
+        loadCounselors();
+    }
+
+    private void loadCounselors() {
+        DefaultTableModel model = (DefaultTableModel) tblCounselors.getModel();
+        model.setRowCount(0); // Clear previous data
+
+        for (Counselor c : controller.getAllCounselors()) {
+            model.addRow(new Object[]{
+                c.getId(),
+                c.getName(),
+                c.getSurname(),
+                c.getEmail(),
+                c.getPhone(),
+                "null", // hide password
+                c.getSpecialisation(),
+                c.isAvailable()
+            });
+        }
+    }
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {
+        String name = txtName.getText();
+        String surname = txtSurname.getText();
+        String email = txtEmail.getText();
+        String phone = txtPhone.getText();
+        String password = new String(txtPassword.getPassword());
+        String specialisation = comboSpecialisation.getSelectedItem().toString();
+        String available = checkAvailable.isSelected() ? "Yes" : "No";
+
+        controller.addCounselor(name, surname, email, phone, password, specialisation, available);
+        loadCounselors();
+    }
+
+    private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {
+        int row = tblCounselors.getSelectedRow();
+        if (row != -1) {
+            int id = Integer.parseInt(tblCounselors.getValueAt(row, 0).toString());
+            String name = txtName.getText();
+            String surname = txtSurname.getText();
+            String email = txtEmail.getText();
+            String phone = txtPhone.getText();
+            String password = new String(txtPassword.getPassword());
+            String specialisation = comboSpecialisation.getSelectedItem().toString();
+            String available = checkAvailable.isSelected() ? "Yes" : "No";
+
+            controller.updateCounselor(id, name, surname, email, phone, password, specialisation, available);
+            loadCounselors();
+        }
+    }
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {
+        int row = tblCounselors.getSelectedRow();
+        if (row != -1) {
+            int id = Integer.parseInt(tblCounselors.getValueAt(row, 0).toString());
+            controller.deleteCounselor(id);
+            loadCounselors();
+        }
+    }
+
+    private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {
+        txtName.setText("");
+        txtSurname.setText("");
+        txtEmail.setText("");
+        txtPhone.setText("");
+        txtPassword.setText("");
+        comboSpecialisation.setSelectedIndex(0);
+        checkAvailable.setSelected(false);
+    }
 }
